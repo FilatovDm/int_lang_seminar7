e1a11ed3ff2ce0c03751b9bfcc1b49a05e8e9671
Dmitriy Filatov <filatov-d@mail.ru>
2022-09-20 20:56:20 +0300

    Example 46 completed

---
+++ Example46/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Задача 46: Задайте двумерный массив размером m×n, 
+заполненный случайными целыми числами.
+m = 3, n = 4.
+1 4 8 19
+5 -2 33 -2
+77 3 8 1
+*/
+
+// Размерность массива
+int n = 0; // кол-во строк
+int m = 0; // кол-во столбцов
+
+Console.Write("Введите количество строк(N): ");
+n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов(M): ");
+m = Convert.ToInt32(Console.ReadLine());
+
+int[,] array = new int[n, m];
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        array[i, j] = new Random().Next(1, 10);
+    }
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        System.Console.Write(array[i, j]);;
+    }
+    System.Console.WriteLine(" ");
+}
